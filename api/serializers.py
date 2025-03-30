2c6f2e87d3e401f18f779e717632e2b0f57d3d91
Jason Ten Broeck <jason.tenbroeck@gmail.com>
2025-03-30 16:56:14 -0500

    Working 03/30/25 v3

--- api/serializers.py
+++ api/serializers.py
@@ -8,11 +8,17 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'is_staff', 'password']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
         return user
 
 class TransportationRequestSerializer(serializers.ModelSerializer):
