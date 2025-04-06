df69ab0a98a8b741e89b43f0f5e50ba09f6c515c
Jason Ten Broeck <jason.tenbroeck@gmail.com>
2025-04-06 17:49:27 -0500

    Working 04/06/25 v1

--- commun_backend/urls.py
+++ commun_backend/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.routes')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', backend_home, name='home'),
