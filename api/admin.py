77acf35ce0c06e8a085eb691ef2f1f6b607a0232
Jason Ten Broeck <jason.tenbroeck@gmail.com>
2025-05-24 18:06:11 -0500

    Working 05/24/25 v9 - GOOD RESTORE

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import (
+    TransportationRequest,
+    MaintenanceRequest,
+    DailyMenu,
+    MealSelection,
+    Activity,
+    ActivityInstance,
+    Alert,
+    WellnessReminder,
+    BillingStatement,
+    UserProfile,
+    Feed,
+)
 
-# Register your models here.
+admin.site.register(TransportationRequest)
+admin.site.register(MaintenanceRequest)
+admin.site.register(DailyMenu)
+admin.site.register(MealSelection)
+admin.site.register(Activity)
+admin.site.register(ActivityInstance)
+admin.site.register(Alert)
+admin.site.register(WellnessReminder)
+admin.site.register(BillingStatement)
+admin.site.register(UserProfile)
+admin.site.register(Feed)
